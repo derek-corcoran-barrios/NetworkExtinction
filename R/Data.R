4679c997f2f3bb3c9f3be2b28ee57ce7205fdb0e
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2018-03-05 21:21:57 -0300

    <Intertidal>

--- R/Data.R
+++ R/Data.R
@@ -5,3 +5,12 @@
 #' @format a network
 #'
 "net"
+
+#' The foodweb of the intertidal zone in central chile
+#'
+#' A trophic network with 107 species present in the  intertidal zone of central Chile.
+#' The food-web was reconstructed from the Kéfi et al. 2015
+#
+#' @format a network
+#' @references Kéfi, Sonia, Eric L. Berlow, Evie A. Wieters, Lucas N. Joppa, Spencer A. Wood, Ulrich Brose, and Sergio A. Navarrete. "Network structure beyond food webs: mapping non‐trophic and trophic interactions on Chilean rocky shores." Ecology 96, no. 1 (2015): 291-303.
+"chilean_intertidal"
--- vignettes/VignetteNetworkExt.R
+++ vignettes/VignetteNetworkExt.R
@@ -10,13 +10,30 @@ knitr::opts_chunk$set(fig.width=6, fig.height=4)
 #  library(NetworkExtinction)
 
 ## ------------------------------------------------------------------------
-#create a matrix
-a<- matrix(c(0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0), nrow=10, ncol=10)
+##To import the matrix from excel:
 
-#the matrix of the data(net) used below is the transpose of a
+#library(readxl)
+#MatrixB <- read_excel(HERE YOU WRITE THE DIRECTORY WHERE YOU EXCEL MATRIX ARE)
+#View(MatrixB)
+
+##Once the matrix was already imported, you need to build a network object, to do this:
+
+#library(network)
+#net <- as.network(MatrixB, loops = TRUE)
+#net 
+
+## ------------------------------------------------------------------------
+#To create the matrix a
+a<- matrix(c(0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0), nrow=10, ncol=10)
+a
+
+#Note that, the previous matrix a, the consumers are in the rows and the resources in the columns. For this reason, you need to  transpose the matrix a. 
+
+#To transpose, this is the form:
 a<- t(a) #transpose matrix a
 a
-#create a network
+
+#Once the matrix is ready, you need to build a network object, to do this:
 library(network)
 net <- as.network(a, loops = TRUE)
 net
@@ -38,7 +55,7 @@ ExtinctionOrder(Network = net, Order = c(2,4,7))
 
 ## ---- message=FALSE------------------------------------------------------
 data(net)
-RandomExtinctions(Network= net, nsim= 10)
+RandomExtinctions(Network= net, nsim= 50)
 
 ## ----message=FALSE, warning=FALSE----------------------------------------
 data("net")
@@ -56,6 +73,16 @@ Comparison$graph
 Comparison$Test
 
 ## ------------------------------------------------------------------------
-data("net")
-degree_distribution(net, name = "Test")
+data(net)
+history <- Mostconnected(Network = net)
+ExtinctionPlot(History = history)
+
+# To specify the variable to be ploted in the y axis, you need to
+# add the name of the variable that you want plot
+ExtinctionPlot(History = history, Variable = "LinksPerSpecies")
+
+
+## ------------------------------------------------------------------------
+data("chilean_intertidal")
+degree_distribution(chilean_intertidal, name = "Test")
 
