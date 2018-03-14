4e6f842fecb37b80a6c0a0d8bd840a0626bc4c4a
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2018-03-13 21:31:18 -0300

    <Fixed vigneete hyphen>

--- R/Data.R
+++ R/Data.R
@@ -9,8 +9,8 @@
 #' The foodweb of the intertidal zone in central chile
 #'
 #' A trophic network with 107 species present in the  intertidal zone of central Chile.
-#' The food-web was reconstructed from the Kéfi et al. 2015
+#' The food web was reconstructed from the Kefi et al. 2015
 #
 #' @format a network
-#' @references Kéfi, Sonia, Eric L. Berlow, Evie A. Wieters, Lucas N. Joppa, Spencer A. Wood, Ulrich Brose, and Sergio A. Navarrete. "Network structure beyond food webs: mapping non‐trophic and trophic interactions on Chilean rocky shores." Ecology 96, no. 1 (2015): 291-303.
+#' @references Kefi, Sonia, Eric L. Berlow, Evie A. Wieters, Lucas N. Joppa, Spencer A. Wood, Ulrich Brose, and Sergio A. Navarrete. "Network structure beyond food webs: mapping non trophic and trophic interactions on Chilean rocky shores." Ecology 96, no. 1 (2015.
 "chilean_intertidal"
