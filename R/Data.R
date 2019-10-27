24ea2e58330591be0ea093879b9f10c03e86c853
derek-corcoran-barrios <derek.corcoran.barrios@gmailcom>
2019-10-27 10:33:54 -0300

    Added Most_Connected and Less_Connected datasets to the package

--- R/Data.R
+++ R/Data.R
@@ -14,3 +14,22 @@
 #' @format a network
 #' @references Kefi, Sonia, Eric L. Berlow, Evie A. Wieters, Lucas N. Joppa, Spencer A. Wood, Ulrich Brose, and Sergio A. Navarrete. "Network structure beyond food webs: mapping non trophic and trophic interactions on Chilean rocky shores." Ecology 96, no. 1 (2015.
 "chilean_intertidal"
+
+#' A densely connected foodweb
+#'
+#' A trophic network with 30 species and 222 trophic interactions.
+#' This foodweb has a connectance of 0.3
+#
+#' @format a network
+#' @seealso \code{\link{Less_Connected}}
+"More_Connected"
+
+#' A sparsely connected foodweb
+#'
+#' A trophic network with 30 species and 47 trophic interactions.
+#' This foodweb has a connectance of 0.03
+
+
+#' @format a network
+#' @seealso \code{\link{More_Connected}}
+"Less_Connected"
